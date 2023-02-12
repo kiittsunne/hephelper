d63e4b52b0e294cdd142d6a3013b1adf445bdccb
Jonathan <jonjosh123@gmail.com>
2023-02-13 00:14:43 +0800

    Create struc.js

---
+++ client/src/data/struc.js
@@ -0,0 +1,17 @@
+const sampleStruc = {
+    "name": "String",
+    "abbreviation": "String",
+    "chemicalFormula": "String",
+    "hep": 180.1, // numeric 
+    "publication": {
+      "journal": "J. Organomet. Chem.",
+      "year": 2007, // numeric
+      "volume": 692, // numeric
+      "pageNumber": "3606–3613"
+    },
+    "donorAtom": "Carbon", // string
+    "formalCharge": 0, // numeric
+    "bondLength": 2.081, // numeric
+    "isomerization": True, // boolean
+    "anagosticInteraction": True // boolean
+  }
